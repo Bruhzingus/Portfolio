fc884725a7af0d6529ce61adeec642d694bdaf0c
bruhzings <tomat@RANDALL_PC>
2025-11-19 20:19:12 -0700

    portfolio 1.9

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,17 @@
 import './Projects.css';
 import minesProject from '../assets/mines.jpg';
+import fitnessProject from '../assets/fitness.jpg';
 function Projects () {
     return (
     <section id="projects">
         <h2>Projects</h2>
         <p>Take a look at some of my recent projects!</p>
         <div className="project">
-          <h3>Portfolio Website</h3>
+          <h3>Coaching Website</h3>
+          <img src={fitnessProject} alt="Fitness Project Image" className="fitness-pic" />
           <p>
-            A personal website built with React and Vite to showcase my IT skills,
-            coding projects, and professional background. Fully utilizes the file storage and web hosting
-             features of GitHub. 
+            A personal coaching website built using Wix and Figma to showcase UI/UX design with a focus on accessibility.
+            I developed prototyping skills and demonstrated live usability testing with my website in front of a live audience.
           </p>
         </div>
         <div className="project">
@@ -18,7 +19,7 @@ function Projects () {
           <img src={minesProject} alt="Mines Project Image" className="mines-pic" />
           <p>
              A C# application that can replicate the famous high stakes game, Mines. 
-            It features a money system, adjustable mine values, and an option to save progress. 
+            It features a money system, adjustable mine values, and accurate risk vs reward scalability. 
           </p>
         </div>
       </section>
